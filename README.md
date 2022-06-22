5a091a3f3327e0f4de7dc992a0ca46a273dfa045
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-06-22 09:27:08 +0200

    adding scripts and assets

---
+++ README.md
@@ -0,0 +1,23 @@
+# CASPT2 RDM Examples
+In this repo you will find multidimensional expectation values (RDMs and CASPT2 intermediates) in the following formats:
+ - `*.npy` are numpy arrays - they are read in and checked for mutual compatability in `checks.py` wherein their index ordering and algebraic convention is also specified
+ - `spinfree*` are of the NECI-style plain text format, extended to higher body RDMs and intermediates. the indices are in normal order
+ - `M7.caspt2.h5` is the M7-style HDF5 archive. Note that subgroups of `archive/rdms` now contain a dataset specifying whether the data are spinfree. Spinfree data can be assumed to be normalized since they are not suitable for restarts. The rows of the indices datasets are also in normal order
+
+The system in question is the ground state of N2. and the methods used are RHF, CASSCF, and CASPT2. The converged CASSCF orbitals are used in the CASPT2 procedure, not the pseudo-canonical ones.
+
+--------------------------------------------
+|Basis set             |cc-pVDZ            |
+--------------------------------------------
+|Density-fitting basis |cc-pVTZ-jkfit      |
+--------------------------------------------
+|Bond length           |1.098 Angstrom     |
+--------------------------------------------
+|No. inactive MOs      |4                  |
+--------------------------------------------
+|No. active MOs        |6                  |
+--------------------------------------------
+|No. secondary MOs     |18                 |
+--------------------------------------------
+
+For more information about the calculation procedure, see the bagel input and output files.
---
+++ checks.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+'''
+load spinfree RDM and CASPT2 intermediate tensors and demonstrate consistency with respect to:
+    - hermiticity
+    - partial traces
+    - Fock contraction (on-the-fly contracted 4F should be equivalent to explicit contraction)
+
+all expectation value arrays (RDMs and PT2 intermediates) are normal-ordered but indexed 
+in creation-annihilation pairs i.e.
+rdm2[a, i, b, j] = sum_s,t <a_s b_t j_t i_s>
+where the sum runs over spin indices
+'''
+
+import numpy as np
+from scipy import special
+
+# ascertain number of orbitals and electrons from the 1RDM
+rdm1 = np.load('rdm1.npy')
+norb = rdm1.shape[0]
+nelec = np.einsum('aa->', rdm1)
+
+# check hermiticity
+assert np.allclose(rdm1, rdm1.transpose(1, 0))
+
+def one_trace_fac(rank):
+    return nelec-rank+1
+
+def comb(n, r):
+    return special.comb(n, r, exact=True)
+
+def trace_fac(rank_senior, rank_junior):
+    out = 1
+    for rank in range(rank_junior+1, rank_senior+1): out*=one_trace_fac(rank)
+    return out
+
+# load 2RDM
+rdm2 = np.load('rdm2.npy')
+# check hermiticity
+assert np.allclose(rdm2, rdm2.transpose(1, 0, 3, 2))
+# check partial traces
+assert np.allclose(np.einsum('ppqq->', rdm2), trace_fac(2, 0))
+assert np.allclose(np.einsum('aipp->ai', rdm2), trace_fac(2, 1)*rdm1)
+assert np.allclose(np.einsum('ppai->ai', rdm2), trace_fac(2, 1)*rdm1)
+
+# load 3RDM
+rdm3 = np.load('rdm3.npy')
+# check hermiticity
+assert np.allclose(rdm3, rdm3.transpose(1, 0, 3, 2, 5, 4))
+# check partial traces
+assert np.allclose(np.einsum('ppqqrr->', rdm3), trace_fac(3, 0))
+assert np.allclose(np.einsum('aippqq->ai', rdm3), trace_fac(3, 1)*rdm1)
+assert np.allclose(np.einsum('aibjpp->aibj', rdm3), trace_fac(3, 2)*rdm2)
+
+# load 4RDM
+rdm4 = np.load('rdm4.npy')
+# check hermiticity
+assert np.allclose(rdm4, rdm4.transpose(1, 0, 3, 2, 5, 4, 7, 6))
+# check partial traces
+assert np.allclose(np.einsum('ppqqrrss->', rdm4), trace_fac(4, 0))
+assert np.allclose(np.einsum('aippqqrr->ai', rdm4), trace_fac(4, 1)*rdm1)
+assert np.allclose(np.einsum('aibjppqq->aibj', rdm4), trace_fac(4, 2)*rdm2)
+assert np.allclose(np.einsum('aibjckpp->aibjck', rdm4), trace_fac(4, 3)*rdm3)
+
+# load on-the-fly contracted Fock-4RDM
+rdm4f = np.load('rdm4f.npy')
+# check hermiticity
+assert np.allclose(rdm4f, rdm4f.transpose(1, 0, 3, 2, 5, 4))
+
+# load generalized Fock matrix
+fock = np.load('fock.npy')
+
+# check contractions
+assert np.allclose(rdm4f, np.einsum('aibjckdl,ai->bjckdl', rdm4, fock))
+assert np.allclose(rdm4f, np.einsum('aibjckdl,bj->aickdl', rdm4, fock))
+assert np.allclose(rdm4f, np.einsum('aibjckdl,ck->aibjdl', rdm4, fock))
+assert np.allclose(rdm4f, np.einsum('aibjckdl,dl->aibjck', rdm4, fock))
